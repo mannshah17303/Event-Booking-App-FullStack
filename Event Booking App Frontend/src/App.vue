9ce7c2c0fc6d9cfa8ce38a9298f84c2d57b805f0
Mann Shah <mann.shah@esparkbizmail.com>
2025-08-04 18:06:18 +0530

    final event booking app with vue js as frontend and sequelize as backend

--- Event Booking App Frontend/src/App.vue
+++ Event Booking App Frontend/src/App.vue
@@ -56,70 +56,160 @@ onMounted(async () => {
   const staticEvents = events.data.data;
   store.dispatch("loadEvents", staticEvents);
 });
+
+const drawer = ref(false);
 </script>
 
 <template>
   <v-app>
-    <v-app-bar app flat elevation="2" class="px-4">
+    <v-app-bar app flat elevation="2" class="px-4 header-bar">
       <v-toolbar-title class="font-weight-bold text-h5 ml-0">
         Your Day, Your Way Events
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        @click="handleClick"
-        variant="outlined"
-        color="block"
-        class="text-none mr-2"
-      >
-        Dashboard
-      </v-btn>
-      <v-btn
-        @click="handleGroupEventsClick"
-        variant="outlined"
-        color="block"
-        class="text-none mr-2"
-      >
-        Group events
-      </v-btn>
-      <v-btn
-        @click="handleMyBookingsClick"
-        variant="outlined"
-        color="block"
-        class="text-none mr-2"
-      >
-        My Bookings
-      </v-btn>
-      <v-btn
-        @click="handleFavoritesClick"
-        variant="outlined"
-        color="block"
-        class="text-none mr-2"
-      >
-        Favorites
-      </v-btn>
-      <v-btn
-        v-if="!loggedInUser"
-        variant="outlined"
-        @click="handleRegisterClick"
-        color="block"
-        class="text-none mr-2"
-        >Login/Register
-      </v-btn>
-      <div v-else-if="loggedInUser" class="flex justify-center align-middle">
+      <div class="nav-buttons">
         <v-btn
+          @click="handleClick"
           variant="outlined"
-          @click="handleLogoutClick"
-          color="red"
+          color="block"
           class="text-none mr-2"
-          >Logout
+        >
+          Dashboard
         </v-btn>
-        <h2 class="font-bold mt-2 mr-1">{{ loggedInUser.name }}</h2>
+        <v-btn
+          @click="handleGroupEventsClick"
+          variant="outlined"
+          color="block"
+          class="text-none mr-2"
+        >
+          Group events
+        </v-btn>
+        <v-btn
+          @click="handleMyBookingsClick"
+          variant="outlined"
+          color="block"
+          class="text-none mr-2"
+        >
+          My Bookings
+        </v-btn>
+        <v-btn
+          @click="handleFavoritesClick"
+          variant="outlined"
+          color="block"
+          class="text-none mr-2"
+        >
+          Favorites
+        </v-btn>
+        <v-btn
+          v-if="!loggedInUser"
+          variant="outlined"
+          @click="handleRegisterClick"
+          color="block"
+          class="text-none mr-2"
+          >Login/Register
+        </v-btn>
+        <div
+          v-else-if="loggedInUser"
+          class="user-info flex justify-center align-middle"
+        >
+          <v-btn
+            variant="outlined"
+            @click="handleLogoutClick"
+            color="red"
+            class="text-none mr-2"
+            >Logout
+          </v-btn>
+          <h2 class="font-bold mt-2 mr-1">{{ loggedInUser.name }}</h2>
+        </div>
       </div>
+
+      <v-btn
+        icon
+        class="hamburger-btn"
+        aria-label="Open navigation menu"
+        @click="drawer = !drawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+
       <i @click="handleCalendarIconClick" class="mdi mdi-calendar"></i>
       <i @click="handleAccountIconClick" class="mdi mdi-account"></i>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app temporary class="mobile-drawer">
+      <v-list nav dense>
+        <v-list-item
+          @click="
+            () => {
+              handleClick();
+              drawer = false;
+            }
+          "
+        >
+          <v-list-item-title>Dashboard</v-list-item-title>
+        </v-list-item>
+        <v-list-item
+          @click="
+            () => {
+              handleGroupEventsClick();
+              drawer = false;
+            }
+          "
+        >
+          <v-list-item-title>Group events</v-list-item-title>
+        </v-list-item>
+        <v-list-item
+          @click="
+            () => {
+              handleMyBookingsClick();
+              drawer = false;
+            }
+          "
+        >
+          <v-list-item-title>My Bookings</v-list-item-title>
+        </v-list-item>
+        <v-list-item
+          @click="
+            () => {
+              handleFavoritesClick();
+              drawer = false;
+            }
+          "
+        >
+          <v-list-item-title>Favorites</v-list-item-title>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item
+          v-if="!loggedInUser"
+          @click="
+            () => {
+              handleRegisterClick();
+              drawer = false;
+            }
+          "
+        >
+          <v-list-item-title>Login/Register</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item
+          v-else-if="loggedInUser"
+          @click="
+            () => {
+              handleLogoutClick();
+              drawer = false;
+            }
+          "
+        >
+          <v-list-item-title
+            >Logout ({{ loggedInUser.name }})</v-list-item-title
+          >
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view />
@@ -141,19 +231,48 @@ onMounted(async () => {
 </template>
 
 <style scoped>
-.mdi-calendar {
-  font-size: 30px;
-}
-.mdi-calendar:hover {
-  cursor: pointer;
+.hamburger-btn {
+  display: none;
 }
 
+.mobile-drawer {
+  display: none;
+}
+
+@media (max-width: 960px) {
+  .nav-buttons {
+    display: none;
+  }
+
+  .hamburger-btn {
+    display: inline-flex;
+  }
+
+  .mobile-drawer {
+    display: block;
+  }
+}
+
+.mdi-calendar {
+  font-size: 30px;
+  cursor: pointer;
+}
 .mdi-account {
   font-size: 27px;
   margin-left: 9px;
+  cursor: pointer;
 }
 
-.mdi-account:hover {
+.user-info h2 {
+  margin: 0;
+  line-height: 36px;
+}
+
+.v-btn--icon {
+  margin-right: 12px;
+}
+
+.v-list-item {
   cursor: pointer;
 }
 </style>
